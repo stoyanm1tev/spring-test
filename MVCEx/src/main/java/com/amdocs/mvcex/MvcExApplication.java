39ea2b9a0cdde59d8da95a3476a6925b052fd8ca
User <User@Lab-637599602718526007-1>
2021-06-24 12:07:39 +0530

    Starting project

---
+++ MVCEx/src/main/java/com/amdocs/mvcex/MvcExApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.mvcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExApplication.class, args);
+	}
+
+}
---
+++ MVCEx/src/test/java/com/amdocs/mvcex/MvcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.mvcex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
